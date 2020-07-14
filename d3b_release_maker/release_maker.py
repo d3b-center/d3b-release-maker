69c0e3131c3dee4a134ecbd9a234a731e05dc276
Avi Kelman <patcherton.fixesthings@gmail.com>
2020-07-14 12:07:26 -0600

    :bug: Order the PR list by when changes were merged

--- d3b_release_maker/release_maker.py
+++ d3b_release_maker/release_maker.py
@@ -166,7 +166,7 @@ class GitHubReleaseNotes:
                     regex.search(release_pattern, p["title"]) is None
                 ):
                     prs.append(p)
-        return prs
+        return sorted(prs, key=lambda x: x["merged_at"], reverse=True)
 
     def _get_commit_date(self, commit_url):
         """
